f6c0f3a79edfa97cfc40fb307825c4f5c5d72324
D3R5 <diegoesteban.ders@gmail.com>
2024-03-26 14:45:55 -0300

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,19 @@
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, nobis
+        debitis cupiditate esse necessitatibus reprehenderit iusto pariatur
+        temporibus aliquid sit iure doloribus at autem ducimus corporis qui ex
+        velit dolore!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, tenetur
+        deleniti! Voluptate accusamus perferendis fugit aspernatur sit,
+        repellendus et tenetur quasi deserunt magni eius tempora, laboriosam
+        nisi, enim culpa odio!
+      </p>
+    </div>
+  );
+}
